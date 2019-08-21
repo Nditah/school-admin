5f61793e13aec73fb544408b0e5055c5f4bd58c9
chiji1 <okaforchijioke01@gmail.com>
2019-08-21 16:06:09 +0100

    created staff component list as example

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,4 +1,5 @@
 
-
+export { Counties } from './counties/counties';
 export { Staffs } from './staffs/staffs';
 export { Admissions } from './admissions/admissions';
+export { States } from './states/states';
