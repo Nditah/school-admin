251fbf693f8c21ac972500269cc74e1abc75e0af
chiji1 <okaforchijioke01@gmail.com>
2019-08-22 11:05:12 +0100

    working on subject create

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -110,14 +110,14 @@ export class ApiService {
   // /////////////////////////////////
 
   getStaff(path): Observable<any> {
-    const url = `${this.env.API_URL}/staff${path}`;
+    const url = `${this.env.API_URL}/staffs${path}`;
     return this.http.get(url, httpOptions).pipe(
       map(this.extractData),
       catchError(this.handleError));
   }
 
   postStaff(data): Observable<any> {
-    const url = `${this.env.API_URL}/staff`;
+    const url = `${this.env.API_URL}/staffs`;
     const payload = this.cleanObject(data);
     return this.http.post(url, payload, httpOptions).pipe(
         catchError(this.handleError)
@@ -125,7 +125,7 @@ export class ApiService {
   }
 
   updateStaff(id: string, data): Observable<any> {
-    const url = `${this.env.API_URL}/staff/${id}`;
+    const url = `${this.env.API_URL}/staffs/${id}`;
     const payload = this.cleanObject(data);
     return this.http.put(url, payload, httpOptions).pipe(
         catchError(this.handleError)
@@ -252,7 +252,7 @@ export class ApiService {
   }
 
   postSubject(data): Observable<any> {
-    const url = `${this.env.API_URL}/subject`;
+    const url = `${this.env.API_URL}/subjects`;
     const payload = this.cleanObject(data);
     return this.http.post(url, payload, httpOptions).pipe(
         catchError(this.handleError)
@@ -260,7 +260,7 @@ export class ApiService {
   }
 
   updateSubject(id: string, data): Observable<any> {
-    const url = `${this.env.API_URL}/subject/${id}`;
+    const url = `${this.env.API_URL}/subjects/${id}`;
     const payload = this.cleanObject(data);
     return this.http.put(url, payload, httpOptions).pipe(
         catchError(this.handleError)
