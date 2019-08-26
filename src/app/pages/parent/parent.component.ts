3ac10b99576f8a97eead6fc841d893afa5297109
IkennaVictor <ikennavictorofficial@gmail.com>
2019-08-26 16:38:42 +0100

    modified staff, student and parent

--- src/app/pages/parent/parent.component.ts
+++ src/app/pages/parent/parent.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Parent } from '../../models';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from '../../services';
+import { Parents } from '../../providers';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  searchForm: FormGroup;
+  currentRecords: Array<Parent>;
+  loading = false;
+  activeSidebar = false;
+  page_name = 'List of Parents';
+
+  constructor(private router: Router,
+              private notify: NotificationService,
+              private parents: Parents) {
+                this.currentRecords = this.parents.query();
+              }
 
   ngOnInit() {
+    this.notify.showNotification('Showing records of Parents', 'info');
+  }
+
+  async search(data) {
+    const queryString = `?q=${data.searchString}`;
+    console.log(data);
+  }
+
+  closeSideBar($event) {
+    this.activeSidebar = $event;
+  }
+
+  goToAdd() {
+    this.router.navigate(['parents/add']);
+  }
+
+  goToDetail(record) {
+    this.router.navigate([`parents/detail/${record.id}`]);
+  }
+
+  goToEdit(record) {
+    this.router.navigate([`parents/edit/${record.id}`]);
+  }
+  removeRecord(record) {
+    // delete method
   }
 
 }
--- src/app/pages/staff/staff-detail/staff-detail.component.ts
+++ src/app/pages/staff/staff-detail/staff-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Staff } from '../../../models';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-staff-detail',
@@ -7,9 +9,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffDetailComponent implements OnInit {
 
-  constructor() { }
+  records: Array<Staff>;
+  record: Staff;
+  response: any;
+  success = false;
+  message = '';
+
+      title: '';
+      surname: '';
+      given_name: '';
+      gender: '';
+      birth_date: '';
+      marital_status: '';
+      phone: '';
+      phone_personal: '';
+      address: '';
+      state: '';
+      county: '';
+      email: '';
+      staff_type: '';
+      classe: '';
+      subject: '';
+      password: '';
+      kin: '';
+      kin_phone: '';
+      kin_address: '';
+      guarantor1: '';
+      guarantor1_phone: '';
+      guarantor1_address: '';
+      guarantor2: '';
+      guarantor2_phone: '';
+      guarantor2_address: '';
+      profession: '';
+      qualification: '';
+      employment_status: '';
+      tax: '';
+      basic_salary: '';
+      bonus: '';
+      entertainment_allowance: '';
+      house_allowance: '';
+      lunch_allowance: '';
+      medical_allowance: '';
+      transport_allowance: '';
+      utility_allowance: '';
+      welfare_allowance: '';
+      pension: '';
+      bank_name: '';
+      bank_account_number: '';
+      bank_account_name: '';
+      rank: '';
+      office: '';
+      role: '';
+      subsidiary: '';
+      remark: '';
+      photo: '';
+      is_salary_payable: '';
+      is_document_complete: '';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
   }
+
+  goToEdit(record: any): void {
+    this.router.navigate([`staff/edit/${record.id}`]);
+  }
+
+  goBack() {
+    window.history.back();
+  }
+
+
+
+
+  // this.id = this.user.id || '';
+  //   this.serial = this.user.serial;
+  //   this.driver_type = this.user.driver_type || '';
+  //   this.driver_licence = this.user.driver_licence || '';
+  //   this.surname = this.user.surname || '';
+  //   this.other_name = this.user.other_name || '';
+  //   this.gender = this.user.gender || '';
+  //   this.birth_date = this.user.birth_date || '';
+  //   this.marital_status = this.user.marital_status || '';
 
 }
